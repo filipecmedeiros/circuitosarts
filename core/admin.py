062143e71be565a4cc508e7f27d5b3453709ef73
Filipe Medeiros <fcma@cin.ufpe.br>
2017-07-25 12:46:20 -0300

    Cadastro de artistas e espaços no heroku

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
+from .models import State, City, Style, Artist, Club
+
 # Register your models here.
+class StateAdmin (admin.ModelAdmin):
+	list_display = ['name']
+	search_fields = ['name']
+
+class CityAdmin (admin.ModelAdmin):
+	list_display = ['name', 'state']
+	search_fields = ['name']
+
+class StyleAdmin (admin.ModelAdmin):
+	list_display = ['gener']
+	search_fields = ['gener']	
+
+class ArtistAdmin (admin.ModelAdmin):
+
+	list_display = ['name', 'gener', 'price', 'contact', 'state', 'city', 'neighborhood', 'address', 'created', 'modified']
+	search_fields = ['name', 'gener__gener', 'price', 'state__name', 'city__name']
+	list_filter = ['created', 'modified']
+
+class ClubAdmin (admin.ModelAdmin):
+
+	list_display = ['name', 'owner', 'opened', 'contact', 'state', 'city', 'neighborhood', 'address', 'created', 'modified']
+	search_fields = ['name', 'owner', 'state__name', 'city__name', 'neighborhood']
+	list_filter = ['created', 'modified']
+
+admin.site.register(State, StateAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Style, StyleAdmin)
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Club, ClubAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,86 @@
 from django.db import models
 
 # Create your models here.
+
+class State (models.Model):
+	name = models.CharField('Estado', max_length=100)
+
+	def __str__ (self):
+		return self.name
+	
+	class Meta:
+		verbose_name = 'Estado'
+		verbose_name_plural = 'Estados'
+		ordering = ['name']
+
+class City (models.Model):
+	name = models.CharField ('Cidade', max_length=100)
+	state = models.ForeignKey('core.State', verbose_name='Estado')
+
+	def __str__ (self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Cidade'
+		verbose_name_plural = 'Cidades'
+		ordering = ['state', 'name']
+
+class Style (models.Model):
+
+	gener = models.CharField ('Gênero', max_length=100)
+	
+	def __str__ (self):
+		return self.gener
+
+	class Meta:
+		verbose_name = 'Gênero'
+		verbose_name_plural = 'Gêneros'
+		ordering = ['gener']
+
+class Artist (models.Model):
+
+	name = models.CharField('Nome', max_length=100)
+	gener = models.ForeignKey('core.Style', verbose_name='Gênero')
+
+	state = models.ForeignKey('core.State', verbose_name='Estado')
+	city = models.ForeignKey('core.City', verbose_name='Cidade')
+	neighborhood = models.CharField ('Bairro', max_length=100)
+	address = models.CharField('Endereço', max_length=200)
+
+	price = models.IntegerField ('Cachê')
+	contact = models.CharField('Contato', max_length=100)
+
+	created = models.DateTimeField ('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__ (self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Artista'
+		verbose_name_plural = 'Artistas'
+		ordering = ['state', 'city', 'neighborhood', 'name']
+
+class Club (models.Model):
+
+	name = models.CharField('Nome', max_length=100)
+	owner = models.CharField('Responsável', max_length=100)
+	opened = models.CharField('Horário de funcionamento', max_length=100)
+
+	state = models.ForeignKey('core.State', verbose_name='Estado')
+	city = models.ForeignKey('core.City', verbose_name='Cidade')
+	neighborhood = models.CharField('Bairro', max_length=100)
+	address = models.CharField('Endereço', max_length=200)
+
+	contact = models.CharField('Contato', max_length=100)
+
+	created = models.DateTimeField ('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	def __str__ (self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Espaço'
+		verbose_name_plural = 'Espaços'
+		ordering = ['state', 'city', 'name']
